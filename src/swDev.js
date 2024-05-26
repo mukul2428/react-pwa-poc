3ee03657a15f22da47a7ebada462565f2e924853
Mukul Raghav <mukulraghav28@gmail.com>
2024-05-26 17:43:56 +0530

    try: added service worker simply

--- src/swDev.js
+++ src/swDev.js
@@ -1,47 +1,10 @@
 /* eslint-disable no-restricted-globals */
-import { Workbox } from "workbox-window";
+// import { Workbox } from "workbox-window";
 
-export default function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    const wb = new Workbox("/service-worker.js");
-
-    wb.addEventListener("installed", async (event) => {
-      if (event.isUpdate) {
-        if (confirm("New app update is available! Click OK to refresh.")) {
-          // Unregister all service workers
-          const registrations =
-            await navigator.serviceWorker.getRegistrations();
-          for (let registration of registrations) {
-            registration.unregister();
-          }
-
-          // Clear all caches
-          if (window.caches) {
-            const cacheNames = await caches.keys();
-            await Promise.all(cacheNames.map((cache) => caches.delete(cache)));
-          }
-
-          // Clear all IndexedDB databases
-          const databases = await indexedDB.databases();
-          for (let dbInfo of databases) {
-            indexedDB.deleteDatabase(dbInfo.name);
-          }
-
-          // Reload the page to apply the new service worker and updates
-          window.location.reload();
-        }
-      }
-    });
-
-    wb.register()
-      .then((registration) => {
-        console.log("Service worker registered:", registration);
-        return registration;
-      })
-      .catch((error) => {
-        console.error("Service worker registration failed:", error);
-      });
-  } else {
-    console.warn("Service workers are not supported in this browser.");
+export default async function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+      let registration = await navigator.serviceWorker.getRegistration();
+      if (!registration)
+          return registration = await navigator.serviceWorker.register('/service-worker.js');
   }
 }
