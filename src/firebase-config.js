ec0a5066e8af4f3671861bd066defb849ae172ea
Mukul Raghav <mukulraghav28@gmail.com>
2024-05-29 15:58:16 +0530

    fix: projectid issue

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCTObgaMgnLGOi2sNJ6ijPFIpQo9hMBS1Q",
+  authDomain: "pwa-poc-3055a.firebaseapp.com",
+  projectId: "pwa-poc-3055a",
+  storageBucket: "pwa-poc-3055a.appspot.com",
+  messagingSenderId: "829575784826",
+  appId: "1:829575784826:web:ee2d68e4ef12aa01ad8ec3",
 };
 const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
